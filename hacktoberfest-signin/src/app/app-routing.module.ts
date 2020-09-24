d0f0fa78d0a4e5bbd6b5310e733f6386b03b543b
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-23 22:24:15 -0400

    routes new Login Component

--- hacktoberfest-signin/src/app/app-routing.module.ts
+++ hacktoberfest-signin/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: WelcomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hacktoberfest-signin/src/app/app.module.ts
+++ hacktoberfest-signin/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/pages/welcome/welcome.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
