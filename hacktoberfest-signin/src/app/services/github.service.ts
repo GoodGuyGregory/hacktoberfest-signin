5faee77dd973c6fbdb79e0beeb34aa2470293c31
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-24 12:58:49 -0400

    adds github service template

--- hacktoberfest-signin/src/app/services/github.service.ts
+++ hacktoberfest-signin/src/app/services/github.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor() {
+
+  }
+  getUserAvatar() {
+    return "returning username.urlAvatar";
+  }
 }
