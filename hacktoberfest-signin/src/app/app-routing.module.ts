b8f2a7411ee5ecc22cc09a8dcfa1569ff2893c92
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-27 01:57:34 -0400

    adds routing for graph component

--- hacktoberfest-signin/src/app/app-routing.module.ts
+++ hacktoberfest-signin/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './components/pages/welcome/welcome.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { GraphComponent } from './components/pages/graph/graph.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'login', component: LoginComponent
+  },
+  {
+    path: 'graph', component: GraphComponent
   }
 ];
 
