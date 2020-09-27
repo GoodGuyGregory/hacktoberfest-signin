5999405c92ffc5b87fdc18dc0447a576846db726
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-27 02:02:55 -0400

    adds add user route to graph component

--- hacktoberfest-signin/src/app/services/languages.service.ts
+++ hacktoberfest-signin/src/app/services/languages.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injectable, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LanguagesService {
 
-  constructor() { }
+
+export class LanguagesData {
+  // Import User Data to component
+  user: string;
+  Java: number;
+  Python: number;
+  Go: number;
+  Cplusplus: number;
+  Sql: number;
+
 }
+
+let languageData: LanguagesData[] = [{
+  user: "GoodGuyGregory",
+  Java: 1,
+  Python: 1,
+  Go: 1,
+  Cplusplus: 1,
+  Sql: 1,
+}, {
+  user: "SamuraiJack",
+  Java: 2,
+  Python: 3,
+  Go: 3,
+  Cplusplus: 5,
+  Sql: 8,
+}, {
+  user: "Jake",
+  Java: 4,
+  Python: 2,
+  Go: 3,
+  Cplusplus: 7,
+  Sql: 9,
+}];
+
+@Injectable()
+export class LanguageService {
+  getLanuageData(): LanguagesData[] {
+    return languageData;
+  }
+}
+
+
