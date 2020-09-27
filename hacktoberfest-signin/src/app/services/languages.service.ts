ec1e0920473e146604a216acaa2f0641dc7210bd
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-27 13:35:55 -0400

    changes graph data to languages

--- hacktoberfest-signin/src/app/services/languages.service.ts
+++ hacktoberfest-signin/src/app/services/languages.service.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class LanguagesData {
   // Import User Data to component
   arg: string;
-  Java: number;
+  java: number;
   Python: number;
   Go: number;
   Cplusplus: number;
@@ -18,28 +18,28 @@ export class LanguagesData {
 
 let languageData: LanguagesData[] = [{
   arg: "Python",
-  Java: 1.0,
+  java: 1.0,
   Python: 1.0,
   Go: 1.0,
   Cplusplus: 1.0,
   Sql: 1.0,
 }, {
   arg: "Java",
-  Java: 2.0,
+  java: 2.0,
   Python: 3.0,
   Go: 3.0,
   Cplusplus: 5.0,
   Sql: 8.0,
 }, {
   arg: "C++",
-  Java: 4.0,
+  java: 4.0,
   Python: 2.0,
   Go: 3.0,
   Cplusplus: 7.0,
   Sql: 9.0,
 }, {
   arg: "F #",
-  Java: 4.0,
+  java: 4.0,
   Python: 2.0,
   Go: 3.0,
   Cplusplus: 7.0,
@@ -48,28 +48,28 @@ let languageData: LanguagesData[] = [{
 },
 {
   arg: "Ruby",
-  Java: 4.0,
+  java: 4.0,
   Python: 2.0,
   Go: 3.0,
   Cplusplus: 7.0,
   Sql: 9.0,
 }, {
   arg: "Swift",
-  Java: 4.0,
+  java: 4.0,
   Python: 2.0,
   Go: 3.0,
   Cplusplus: 7.0,
   Sql: 9.0,
 }, {
   arg: "Go",
-  Java: 4.0,
+  java: 4.0,
   Python: 2.0,
   Go: 3.0,
   Cplusplus: 7.0,
   Sql: 9.0,
 }, {
   arg: "Javascript",
-  Java: 4.0,
+  java: 4.0,
   Python: 2.0,
   Go: 3.0,
   Cplusplus: 7.0,
