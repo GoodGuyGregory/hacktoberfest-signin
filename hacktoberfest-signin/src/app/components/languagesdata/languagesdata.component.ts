238f81d4d8bfbb7405e8705fb120b96afd46d5e9
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-27 12:19:19 -0400

    adds languagedata component with DevExtreme

--- hacktoberfest-signin/src/app/components/languagesdata/languagesdata.component.ts
+++ hacktoberfest-signin/src/app/components/languagesdata/languagesdata.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { DxPolarChartModule } from 'devextreme-angular';
+
+// Services
+import { LanguagesData, LanguageService } from '../../services/languages.service';
 
 @Component({
   selector: 'app-languagesdata',
   templateUrl: './languagesdata.component.html',
-  styleUrls: ['./languagesdata.component.scss']
+  styleUrls: ['./languagesdata.component.scss'],
+  providers: [LanguageService]
 })
-export class LanguagesdataComponent implements OnInit {
 
-  constructor() { }
+export class LanguagesdataComponent implements OnInit {
+  // gets service data
+  languageData: LanguagesData[];
+
+  constructor(private languageService: LanguageService) {
+    // set service data
+    this.languageData = languageService.getLanguageData();
+  }
 
   ngOnInit(): void {
+
   }
 
 }
+
+
+
